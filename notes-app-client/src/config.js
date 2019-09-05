338b7b12c6841ab5e176b0ff609f415ae757f65f
Esau Abraham <esau.seattle@gmail.com>
2019-09-05 04:01:52 -0700

    Aplofy Aws

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -3,6 +3,15 @@ export default {
         REGION: "us-west-1",
         BUCKET: "notes-items-upload"
     },
-    apiGateway: "us-east-2",
+    apiGateway: {
+        REGION:"us-east-2",
+        URL: "https://96n1f6cm1i.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_JncupQzO2",
+        APP_CLIENT_ID: "38etm1lp3d6brcqtlukjca2jma",
+        IDENTITY_POOL_ID:"us-east-2:2f1ef5e1-4176-4f3e-9766-91e1856d5934"
+    }
     
-}
+};
